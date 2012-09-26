06574dce2c7db379e36338c22bd7adc6dffa0b9f
Raul U Cardenas <rucardenas@utexas.edu>
2012-09-25 21:17:43 -0500

    Heap Implementation .40 / Naive 3.28 closes #21

--- PFD.py
+++ PFD.py
@@ -13,11 +13,11 @@ size = [0]
 visited = []
 dependencies_list = []
 target = -1
-
+targets = []
 
 
 # ------------
-# pfd_read
+# pfd_initialize
 # ------------
 
 def pfd_initialize (r, a) :
@@ -28,6 +28,10 @@ def pfd_initialize (r, a) :
 	return true if that succeeds, false otherwise
 	"""
 	s = r.readline()
+	if s == "\n" :
+		s = r.readline()
+		if s == "" :
+			return False
 	if s == "" :
 		return False
 	l = s.split()
@@ -38,32 +42,40 @@ def pfd_initialize (r, a) :
 	assert a[0] > 0
 	assert a[1] > 0
 	size[0] = a[0]
-	#print "Size...", size
 	global matrix
 	global dependencies_list
 	dependencies_list = [0]*size[0]
 	matrix = []*size[0]
-	global visited
-	visited = [0]*size[0]
+
+	assert len(dependencies_list) > 0
+
 	for i in xrange(a[0]):
 		matrix.append([0]*a[0])
 
+	assert len(matrix) > 0 		
+
 	for i in xrange(a[1]):
 		s = r.readline()
-
 		l = s.split()
 
 		row_index = int(l[0])
-
 		num_dependencies = int(l[1])
-
 		dependencies_list[row_index-1]= num_dependencies
 
 		for  n in xrange(2,len(l)):
 			matrix[row_index-1][int(l[n])-1] = 1
+
 	return True
 
+# ------------
+# pfd_clear
+# ------------
+
 def pfd_clear(n):
+	"""
+	n is the column to clear out
+	return array of affected rows
+	"""
 	global matrix
 	r = []
 	for i in xrange(size[0]):
@@ -72,21 +84,6 @@ def pfd_clear(n):
 			r.append(i)
 	return r
 
-def pfd_find_target() :
-	target = -1
-	global matrix
-	for i in xrange(size[0]):
-		
-		found = False
-		for n in xrange(size[0]):
-			
-			if matrix[i][n] == 1:
-				found = True
-		if not found and visited[i] == 0:
-			target = i
-			return target
-
-	return target
 
 def pfd_printd () :
 	global dependencies_list
@@ -102,46 +99,64 @@ def pfd_print () :
 		temp += "\n"
 	print temp
 
+
+# ------------
+# pfd_find_first_target
+# ------------
 def pfd_find_first_target():
+	"""
+	Fills array with all the zero-dependency
+	"""
 	global dependencies_list
-	for i in dependencies_list:
+	global targets
+	num_dependencies = len(dependencies_list)
+	for i in xrange(num_dependencies):
 		if dependencies_list[i] == 0:
-			return i
-	return -1
+			heapq.heappush(targets, i)
 
 # -------------
-# collatz_solve
+# pfd_solve
 # -------------
 
 def pfd_solve (r, w) :
-
+	"""
+	r is a reader
+	w is a writer
+	executes algorithm
+	"""
+	global targets
 	a = [0, 0]
-	pfd_initialize(r, a)
-	targets_array = []
-	target = pfd_find_first_target()
-	targets_array.append(target)
-	result = ""
-	counter = 0
-	
-	temp_array = []
 
+	while pfd_initialize(r, a) :
 
+		#pfd_initialize(r, a)
+		targets_array = []
+		pfd_find_first_target()
+		
+		resultArr = []
 
-	global visited
-	#pfd_printd ()
-	while counter < size[0]:
-		no_dependencies = pfd_find_target()
-		#print "No Dependency: ", no_dependencies
-		pfd_clear(no_dependencies)
-		visited[no_dependencies]= 1
-		result +=  str(no_dependencies+1)
-		counter += 1
-		if counter <size[0]:
-			result += " "
-		#pfd_print()
-	w.write(result + "\n")
+		while len(targets) > 0:
+			target = heapq.heappop(targets)
+			resultArr.append(target+1)
+			new_targets = pfd_clear(target)
 
-	#pfd_print()
-    	
+			for i  in new_targets:
+				dependencies_list[i]-=1
+				if dependencies_list[i] == 0:
+					heapq.heappush(targets,i)
+					
+		#Prints the result
+		for i in xrange(len(resultArr)) :
+		  print resultArr[i],
+		resultArray = []
+		print "\n"
+
+# -------------
+# getMatrix
+# -------------   	
 def getMatrix():
+	"""
+	returns global matrix
+	"""
 	return matrix
+	
